9671574af7e998b29af7132c1c0cc74179600333
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-04 15:39:32 -0500

    se hizo el hook para obtener los gifs

--- src/components/Gifs.jsx
+++ src/components/Gifs.jsx
@@ -1,32 +1,30 @@
-import { useEffect, useState } from "react"
-import { getGifs } from "../helpers/getGifs"
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 
 export const Gifs = ({categoria}) => {
 
 
-    const [images, setImages] = useState([]);
-
-    const getImages = async() => {
-        const newImages = await getGifs(categoria);
-        setImages(newImages);
-    }
-
-
-    useEffect(() => {
-        getImages()
-    }, [])
+    const { images, isLoading } = useFetchGifs(categoria);
     
-
-
 
   return (
     <>
-        <h2>{categoria}</h2>
+        {/* <h2>{categoria}</h2>
+        {
+            isLoading && ( <h2>Cargando 😘</h2> )
+        } */}
+        {
+            isLoading ? (<h2>Cargando 😘</h2>) : (<h2>{categoria}</h2>) 
+        }
+        
+
+
         <div className="container">
-            {images.map( ({id, title, url}) => (
-                <GifItem key={id} url={url} title={title}/>
+            {images.map( ( image ) => (
+                <GifItem
+                    key={ image.id} 
+                    {...image}/>
             ))}
         </div>
     </>
